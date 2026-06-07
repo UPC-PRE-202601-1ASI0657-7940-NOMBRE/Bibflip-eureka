3338f774d82ba62e0af4676a8ab27152a13a217d
P1er0VL <u20211a620@upc.edu.pe>
2026-06-06 20:07:02 -0500

    feat: add Eureka server configuration

---
+++ src/test/java/pe/upc/edu/bibflipbackend/eureka/BibflipEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.edu.bibflipbackend.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibflipEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
